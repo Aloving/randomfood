33e146a24ba3f962846ef0815b6f7e4f645d4b6c
Aloving <Ilegend2011@gmail.com>
2017-06-27 17:07:29 +0400

    search-button action

--- client/src/app/components/layout-components/search-button/search-button.component.ts
+++ client/src/app/components/layout-components/search-button/search-button.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { RecipesService } from '../../../shared/services/recipes/recipes.service';
 
 import { Recipe } from '../../../shared/models/recipe';
@@ -9,9 +10,11 @@ import { Recipe } from '../../../shared/models/recipe';
 	styleUrls: ['./search-button.component.scss'],
 })
 export class SearchButtonComponent {
-	constructor(private recipesService: RecipesService) {}
+	constructor(private recipesService: RecipesService, private router: Router) {}
 
 	onClick() {
-		this.recipesService.getRecieptsCount().subscribe(console.log)
+		this.recipesService
+			.getRecieptsCount()
+			.subscribe(id => this.router.navigate(['/recipe', id]));
 	}
 }
